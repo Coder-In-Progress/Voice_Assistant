6a2d5f6b29699515c584a7dc289da8facef5eeb2
Coder-In-Progress <79266681+Coder-In-Progress@users.noreply.github.com>
2021-04-15 20:47:05 -0400

    Update code.py

--- code.py
+++ code.py
@@ -13,59 +13,39 @@ import pytz
 import subprocess
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-MONTHS = ['January', 'febuary', 'march', 'april', may', 'june', 'july', 'august', 'september', 'november', 'december']
-DAYS = [monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-DAY_ABRE = ["rd", "th", "st", "nd"]
+MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october","november", "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
-
-
 def get_audio():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-			audio = r.listen(source)
-			said = ""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
 
-			try:
-				said = r.recognize_google(audio)
-				print(said)
-			except Esception as e:
-				print("Exception: " + str(e))
-	return said.lower()
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said.lower()
 
 
-speak ("Hello Jithesh")
-get_audio()
-
-text = get_audio()
-
-if "hello" in text:
-	speak("hello")
-	
-if "what is your favorite color" in text:
-	speak("red")
-	
-if "who is the greatest person in the entire universe" in text:
-	speak("Jithesh")
-	
-if "what is your name" in text:
-	speak("Jbob!")
-	
 def authenticate_google():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -73,120 +53,134 @@ def authenticate_google():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
 
-	return service
-def get_events(day, service)
+    return service
+
+
+def get_events(day, service):
     # Call the Calendar API
-	date = datetime.datetime.combine(day, datetime.datetime.min.time())
-	end_date = datetime.datetime.combine(day, datetime.datetime.max.time())
-	est = pytz.EST
-	date = date.astimezone(est)
-	end_date = end_date.astimezone(est)
-	
-    
- 	events_result = service.events().list(calendarID='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat()                              
-				       	singleEvents=True,
+    date = datetime.datetime.combine(day, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(day, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone(utc)
+    end_date = end_date.astimezone(utc)
+
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat(),
+                                        singleEvents=True,
                                         orderBy='startTime').execute()
- events = events_result.get('items', [])
+    events = events_result.get('items', [])
 
     if not events:
         speak('No upcoming events found.')
-    else:  
-	speak(f "You have(len(events)) events on this day.") 
-	
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-	start_(time = str(sart.split("T")[1].split("-")[0])
-	if int(start_time.split(":")[0]) < 12:
-	       start_time = start_time + "am"
-	else:
-	       start_time = str(int(start_time.split(":")[0])-12) + start_time.split(":")[1]
-	       start_time = start_time + "pm"
-	
-	speak(event["summary"] + " at " + start_time)
+    else:
+        speak(f"You have {len(events)} events on this day.")
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+            start_time = str(start.split("T")[1].split("-")[0])
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "am"
+            else:
+                start_time = str(int(start_time.split(":")[0])-12) + start_time.split(":")[1]
+                start_time = start_time + "pm"
+
+            speak(event["summary"] + " at " + start_time)
+
 
 def get_date(text):
-	text=text
-	today = datetime.date.today()
-	
-	if text.count ("today") > 0:
-		return today
-		
-	day = -1
-	day_of_week = -1
-	month = -1
-	year = today.year
-	
-	for word in text.split()
-		if word in months:
-			month = MONTHS.index(month) + 1
-		elif word in months:
-			days_of_week = DAYS.index(word)
-		elif word.isdigit():
-			day = int(word)
-		else:
-			for ext in DAY_ABRE:
-			found = word.find(ext)
-			if found > 0:
-				try:
-					day = int(word[:found])
-				except:
-					pass
-if month < today.month and month = -1:
-if day < today.day and month == -1 and day 1= -1
-if month == -1 and day == -1 and day_of_week 1: -1
-	current_day_of_week - today.weekday()
-	dif = day_of_week = current_day_of_week
-	
-	if dif < 0:
-	     dif += 7
-	     if text.count("next") >= -1
-	     	dif += 7
-	return today + datetime.timedelta(dif)
-if month == -1 or day == -1:
-	return None
-return datetime.date(month=month, day=day, year=year)
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return today
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTIONS:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    # THE NEW PART STARTS HERE
+    if month < today.month and month != -1:  # if the month mentioned is before the current month set the year to the next
+        year = year+1
+
+    # This is slighlty different from the video but the correct version
+    if month == -1 and day != -1:  # if we didn't find a month, but we have a day
+        if day < today.day:
+            month = today.month + 1
+        else:
+            month = today.month
+
+    # if we only found a dta of the week
+    if month == -1 and day == -1 and day_of_week != -1:
+        current_day_of_week = today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+
+    if day != -1:  # FIXED FROM VIDEO
+        return datetime.date(month=month, day=day, year=year)
 
 def note(text):
-	date = datetime.datetime.now()
-	file_name = str(date).replace(":","-") + "-note.txt"
-	with open(file_name, "w") as f:
-	       f.write(text)
-	       
-	  
-	subprocess.Popen(["notepad.exe", file_name])
-		       
-WAKE = "Jbob"
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(file_name, "w") as f:
+        f.write(text)
+
+    subprocess.Popen(["notepad.exe", file_name])
+
+
+WAKE = "hey Jithesh"
 SERVICE = authenticate_google()
 print("Start")
 
 while True:
-	print("Listening")       
-	text= get_audio()
-	
-	if text.count(WAKE) > 0:
-		speak ("Yes?")
-	        text = get_audio()
-	       
-	CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
-	for phrase in CALENDAR_STRS:
-		if phrase in text:
-		       date = get_date(text)
-		       if date:
-		       	get_events(get_date(text), SERVICE)
-		       else:
-		       	speak("Please Try Again, I do not understand")
-	       
-	NOTE_STRS = ["make a note", "write this down", "remember this"]
-	for phrase in NOTE_STRS:
-		if phrase in text:
-		       speak("What would you like me to write down?")
-		       note_text = get_audio()
-		       note(note)
-		       speak("I've made a note")
+    print("Yes?")
+    text = get_audio()
+
+    if text.count(WAKE) > 0:
+        speak("Yes?")
+        text = get_audio()
+
+        CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
+        for phrase in CALENDAR_STRS:
+            if phrase in text:
+                date = get_date(text)
+                if date:
+                    get_events(date, SERVICE)
+                else:
+                    speak("I don't understand")
+
+        NOTE_STRS = ["make a note", "write this down", "remember this"]
+        for phrase in NOTE_STRS:
+            if phrase in text:
+                speak("What would you like me to write down?")
+                note_text = get_audio()
+                note(note_text)
+                speak("I've made a note of that.")

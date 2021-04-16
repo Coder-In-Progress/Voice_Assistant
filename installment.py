488df509df69c2b57a20cb3ffead696446d4c3ca
Coder-In-Progress <79266681+Coder-In-Progress@users.noreply.github.com>
2021-04-15 20:34:12 -0400

    Update installment.py

--- installment.py
+++ installment.py
@@ -4,4 +4,4 @@ pip install playsound
 pip install pyaudio
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthli
 pip install datetime
-pip install pyttx3
+pip install pyttsx3
